91cc7b33e539abf692722ddf426931326a95f8b9
王杰 <wangjie@icaremgt.com>
2019-06-10 16:18:05 +0800

    build(spring-boot-docker): 项目构建，初始提交

--- src/main/java/com/github/springbootdocker/SpringBootDockerApplication.java
+++ src/main/java/com/github/springbootdocker/SpringBootDockerApplication.java
@@ -3,6 +3,11 @@ package com.github.springbootdocker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *  demo
+ * @author   Vangelis
+ * @date 2018/8/23 16:01
+ */
 @SpringBootApplication
 public class SpringBootDockerApplication {
 
